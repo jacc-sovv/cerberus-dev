5eb957f5c3be1a9f3f175aa431bf18fdfe996cf3
MarioEmporium <jack.w.sovereign@gmail.com>
2022-11-30 19:28:25 -0700

    Added final definitions

--- core/jack_update/makekeys.h
+++ core/jack_update/makekeys.h
@@ -49,8 +49,11 @@ int secretkey(struct ecc_private_key *privkey, struct ecc_public_key *pubkey, ui
 /**
  * Uses AES-GCM encryption to encrypt a message into ciphertext using a secret key
  * @param msg A plaintext message you would like to encrypt
+ * @param msg_size The size of the plaintext message
  * @param secret A secret key to use for encryption
+ * @param secret_length The size of the secret key
  * @param AESIV An IV to use for encryption. A 12-byte IV is best (meets NIST standards)
+ * @param AESIV_SIZE The size of the IV used for encryption
  * @param tag The buffer to hold the GCM authentication tag. All tags will be 16 bytes
  * @param ciphertext The buffer to hold the encrypted data. The ciphertext will be the same length as the plaintext
  * @param state An int to hold the numerical value of the state
@@ -61,27 +64,33 @@ int encryptionPID(uint8_t *msg, size_t msg_size, uint8_t *secret, size_t secret_
 /**
  * A function to generate a random string as a OTP, and encrypt that OTP
  * @param secret The secret key to encrypt the OTP with
+ * @param secret_size The size of the secret key
  * @param AESIV An IV to use for encryption. A 12-byte IV is best (meets NIST standards)
+ * @param AESIV_SIZE The size of the IV used for encryption
  * @param tag The buffer to hold the GCM authentication tag. All tags will be 16 bytes
  * @param OTP A buffer to hold a randomly generated OTP into
+ * @param OTPSize The size the OTP should be
  * @param OTPs A buffer to hold the encrypted OTP in
  * @param state An int to hold the numerical value of the state
  * @return 1 on success
 */
-int OTPgen(uint8_t *secret,  size_t secret_size, uint8_t *AESIV, size_t aesiv_size, uint8_t *tag, uint8_t *OTP, size_t OTPSize, uint8_t *OTPs, int *state);
+int OTPgen(uint8_t *secret,  size_t secret_size, uint8_t *AESIV, size_t AESIV_SIZE, uint8_t *tag, uint8_t *OTP, size_t OTPSize, uint8_t *OTPs, int *state);
 
 /**
  * Decrypts an encrypted OTP and compares it to a 
  * @param secret The secret key used to decrypt OTPs
+ * @param secret_size The size of the secret key
  * @param AESIV An IV to use for decryption. Must be the same as the IV provided to encrypt
+ * @param AESIV_SIZE The size of the IV used for encryption
  * @param tag The GCM tag for ciphertext
  * @param OTPs Encrypted OTP to be decrypted
+ * @param OTPs_size The size of the encrypted OTP
  * @param valOTP OTP to be validated against the decrypted OTP
  * @param result A boolean value to check whether the OTP was successfully validated
  * @param An int to hold the numerical value of the state
  * @return 1 on success
 */
-int OTPvalidation(uint8_t * secret, size_t secret_size, uint8_t *AESIV, size_t AESIV_size, uint8_t *tag, uint8_t *OTPs, size_t OTPs_size, uint8_t *valOTP, bool *result, int *state);
+int OTPvalidation(uint8_t * secret, size_t secret_size, uint8_t *AESIV, size_t AESIV_SIZE, uint8_t *tag, uint8_t *OTPs, size_t OTPs_size, uint8_t *valOTP, bool *result, int *state);
 
 /**
  * Unlocks the device and sets the state appropriately
